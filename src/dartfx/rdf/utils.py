2185cf65179456e05b14016bdd34c3571c968b5c
kulnor <pascal.heus@postman.com>
2024-09-19 11:57:51 -0600

    Moved get_rdf_graph_statistics from DDI package

--- src/dartfx/rdf/utils.py
+++ src/dartfx/rdf/utils.py
@@ -1,7 +1,52 @@
 
 from abc import ABC, abstractmethod
 import uuid
+
+from rdflib import Graph
 from .rdf import Uri
+
+def get_rdf_graph_statistics(g: Graph):
+    stats = {}
+    # count triples
+    query = """
+    SELECT (COUNT(*) AS ?n)
+    WHERE {
+        ?s ?p ?o
+    }
+    """
+    for row in g.query(query):
+        stats["n_triples"] = int(str(row.n))
+
+    # count type instances
+    query = """
+    SELECT ?type (COUNT(?instance) AS ?n)
+    WHERE {
+        ?instance a ?type .
+    }
+    GROUP BY ?type
+    ORDER BY ?type
+    """
+    stats["types"] = {}
+    for row in g.query(query):
+        type = str(g.namespace_manager.qname(row.type))
+        stats["types"][type] = {}
+        stats["types"][type]["count"] = int(str(row.n))
+
+    # count type properties instances
+    query = """
+    SELECT ?property (COUNT(?property) AS ?n)
+    WHERE {
+    ?resource a ?type .
+    ?resource ?property ?value .
+    }
+    GROUP BY ?property
+    ORDER BY ?property
+    """
+    stats["properties"] = {}
+    for row in g.query(query):
+        property = str(g.namespace_manager.qname(row.property))
+        stats["properties"][property] = {}
+        stats["properties"][property]["count"] = int(str(row.n))
 
 
 #
