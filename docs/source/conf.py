e9ff277b10a841b7bc14c35db477910ab5311c2e
kulnor <pascal@codata.org>
2025-11-06 10:47:26 -0700

    Fix search path for autogen in Sphinx configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ release = '0.1.0'
 #
 # Add search path for autogen
 #
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../src"))
 
 
 # -- General configuration ---------------------------------------------------
